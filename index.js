add50b3e054c8d0207b50492e37347ec81854aa4
Nagabhushan Adiga <68003831+NagabhushanAdiga@users.noreply.github.com>
2021-08-26 17:32:17 +0530

    about page  created

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 http.createServer((req,res)=>{
     console.log(res)
-    res.write("<h1>hey there</h1>");
+    res.write("<h1>hey there about feature</h1>");
     res.end()
 })
 .listen(8080)
